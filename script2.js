f5325074a781e1d7f5896697a1d7c05c4bcae585
Beubeu457 <95020934+Beubeu457@users.noreply.github.com>
2021-11-25 09:50:29 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("#coco");
+btn.onclick = azerty;
+function azerty() {
+	let nine = prompt("Entrez le code");
+	if (nine == "azerty") {
+		window.scrollbars.vi
+	} else {
+		alert("Code incorrect");
+	}
+}
